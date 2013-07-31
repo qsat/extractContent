83b95c05b52115bf5be9fe4825355ad50235d30e
s-arai <macbookpro@9105-109204.local>
2013-07-31 10:50:24 +0900

    ignore追加など

--- extractContent.rb
+++ extractContent.rb
@@ -6,19 +6,13 @@
 
 
 require "nokogiri"
+require 'fileutils'
+require 'kconv'
 
 class ExtractContent
-  def initialize
-    Dir.chdir File.expand_path File.dirname($0)
-
-    @selectors = [
-      "body > #body",
-      "table[width='100%'] td[valign='top'] > table[width='580']",
-      "table[width='100%'] td[valign='top'] > table[width='540']",
-    ]
-  end
 
   def exec(src, dest, list)
+    Dir.chdir File.expand_path File.dirname($0)
 
     found = {}
     result = {}
@@ -40,25 +34,21 @@ class ExtractContent
 
       doc = Nokogiri::HTML.parse IO.read file, encoding: Encoding::EUC_JP
 
-      txt = ""
-      txt << doc.css("body > #body").length.to_s
-      txt << doc.css("body > #wrapper").length.to_s
-      txt <<  doc.css("td[valign='top'] > table[width='580']").length.to_s
-      txt << doc.css("table[width='100%'] td[valign='top'] > table[width='540']").length.to_s
-      txt << doc.css("table[width='100%'] td[valign='top'] > table[width='750']").length.to_s
-      txt << doc.css("table[width='100%'] td[valign='top'] > table[width='738']").length.to_s
-      txt << doc.css("table[width='100%'] td[valign='top'] table[width='600']").length.to_s
-      #hidenavi, headerline削除してbodyの内容を抽出
-      txt << doc.css("body > table[width='620']").length.to_s
-      #hidenavi, headerline削除してbodyの内容を抽出
-      txt << doc.css("body > table[width='520']").length.to_s
+      elem = doc.css("#hiddennavi, #headerline, #endline1, #endline2, #endline3")
       
-      if txt.match /^0+$/
-        unless file.match /xbanner|chtml|php/
-          p txt  + " " + path
-        end
+      elem.remove()
+
+      str = doc.css("body").inner_html
+
+      dir = dest + File::dirname(path)
+      FileUtils.mkdir_p dir
+      begin
+        File.write dest+path, str.encode( "UTF-8", "EUC-JP" )
+      rescue Encoding::UndefinedConversionError => e
+        File.write dest+path, str
+        p e, path
       end
-      p txt  + " " + path
+
 
       # nakahara, au 以外はディレクトリ変更なし。
 
@@ -70,16 +60,3 @@ end
 ins = ExtractContent.new
 ins.exec "./src", "./dest", "path.txt"
 
-
-### body以下をそのままコピー
-#support/setup/mansion/manual.html
-#en/setting_mannual.html
-#shared/html/assisance.html
-#sys/tag/tag_help.html
-#userpage/road/index.html
-#support/setup/internet/cgi/246_guest_example.html
-#userpage/road/index.html
-#itscom-ch/access/go/lh-tekuteku.html redirect
-#userpage/go/train.html  redirect
-#signup/index.html
-#contents/police/index.html
